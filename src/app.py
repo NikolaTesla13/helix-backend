11cdb708067f2d36fac911288d3826824ff2f368
Asandei Stefan <asandei.stefanel@gmail.com>
2023-05-08 17:07:28 +0300

    cors

--- src/app.py
+++ src/app.py
@@ -1,14 +1,19 @@
 from src.rce import RemoteCodeExecution
 from flask import Flask, request
+from flask_cors import CORS, cross_origin
 
 app = Flask(__name__)
+cors = CORS(app)
 rce_engine = RemoteCodeExecution()
+
+app.config['CORS_HEADERS'] = 'Content-Type'
 
 @app.get("/")
 def index():
     return "<p>Hello, World!</p>"
 
 @app.post("/rce/run")
+@cross_origin()
 def run():
     body = request.get_json(silent=True)
     unsafe = request.args.get('unsafe')
@@ -17,9 +22,10 @@ def run():
         result = rce_engine.unsafe_execute_code(body["code"], body["input"])
     else:
         result = rce_engine.execute_code(body["code"], body["input"])
-        
+
     return result
 
 @app.post("/rce/test")
+@cross_origin()
 def test():
     return "TODO"
